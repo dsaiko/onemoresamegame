26e1fa47acf31b5bb1000baeb173ec86523b6bbf
Dusan Saiko <dusan.saiko@gmail.com>
2014-08-28 00:14:22 +0200

    Fixing debian vs ubuntu installation package

--- dist/deb/300-package.sh
+++ dist/deb/300-package.sh
@@ -22,9 +22,9 @@ cp -R ${DEBUILD_SCRIPT_DIR}/debian/* .
 
 if [ -z "$(dpkg-vendor --derives-from Ubuntu && echo yes)" ]
 then
-	DIST_DEPENDS="qml-module-qtquick-controls (>=5.2.1), qml-module-qtquick-particles2 (>=5.2.1), qml-module-qtquick-localstorage (>=5.2.1)"
+	DIST_DEPENDS="qml-module-qtquick-controls (>=5.2.1), qml-module-qtquick-particles2 (>=5.2.1), qml-module-qtquick-localstorage (>=5.2.1), qml-module-qtgraphicaleffects"
 else
-	DIST_DEPENDS="qtdeclarative5-controls-plugin (>=5.2.1), qtdeclarative5-particles-plugin (>=5.2.1), qtdeclarative5-localstorage-plugin (>=5.2.1)"
+	DIST_DEPENDS="qtdeclarative5-controls-plugin (>=5.2.1), qtdeclarative5-particles-plugin (>=5.2.1), qtdeclarative5-localstorage-plugin (>=5.2.1), libqt5qml-graphicaleffects"
 fi
 
 sed -i "s/\${dist:Depends}/${DIST_DEPENDS}/" control
