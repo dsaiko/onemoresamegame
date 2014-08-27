5a7b0664f0b0fa08ce86929bf522ac25b4b0e8f0
Dusan Saiko <dusan.saiko@gmail.com>
2014-08-27 23:56:22 +0200

    Creating development version: 0.9.12-SNAPSHOT

--- platform-details.h
+++ platform-details.h
@@ -17,7 +17,7 @@ class PlatformDetails : public QObject
 
 public:
 
-    #define APP_BUILD_VERSION "0.9.11"
+    #define APP_BUILD_VERSION "0.9.12-SNAPSHOT"
 
     PlatformDetails(QObject *parent) :QObject(parent), settings("OneMoreSamegame", "onemoresamegame")
     {
